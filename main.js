b3cb6148a6192de2f54ebf0e6d3fd959a7feddfe
SJ <stephane.jacquet@analogway.com>
2017-04-10 11:12:06 +0200

    modification du main

--- main.js
+++ main.js
@@ -75,42 +75,19 @@ autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
 })
 autoUpdater.on('update-available', (ev, info) => {
-  dialog.showMessageBox({
-    type: 'info',
-    title: 'Found Updates',
-    message: 'Found updates, do you want update now?',
-    buttons: ['Sure', 'No']
-  }, (buttonIndex) => {
-    if (buttonIndex === 0) {
-      autoUpdater.downloadUpdate()
-    }
-    else {
-      updater.enabled = true
-      updater = null
-    }
-  })
+  sendStatusToWindow('Update available.');
 })
 autoUpdater.on('update-not-available', (ev, info) => {
-  dialog.showMessageBox({
-    title: 'No Updates',
-    message: 'Current version is up-to-date.'
-  })
-  updater.enabled = true
-  updater = null
+  sendStatusToWindow('Update not available.');
 })
 autoUpdater.on('error', (ev, err) => {
-  dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+  sendStatusToWindow('Error in auto-updater.');
 })
 autoUpdater.on('download-progress', (ev, progressObj) => {
   sendStatusToWindow('Download progress...');
 })
 autoUpdater.on('update-downloaded', (ev, info) => {
-  dialog.showMessageBox({
-    title: 'Install Updates',
-    message: 'Updates downloaded, application will be quit for update...'
-  }, () => {
-    autoUpdater.quitAndInstall()
-  })
+  sendStatusToWindow('Update downloaded; will install in 5 seconds');
 });
 app.on('ready', function() {
   // Create the Menu
